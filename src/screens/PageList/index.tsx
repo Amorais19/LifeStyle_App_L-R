b35822e0a462fee22fadf2f58d8985793d9a90af
Amorais19 <alicemoraisams@gmail.com>
2024-08-07 16:01:11 -0300

    min

--- src/screens/PageList/index.tsx
+++ src/screens/PageList/index.tsx
@@ -1,15 +1,47 @@
-import { ImageBackground, View, Text } from 'react-native';
+import { ImageBackground, View, Text, FlatList } from 'react-native';
 import { styleContainer, colors } from "../../styles/globalstyles";
+import { useEffect, useState } from 'react';
+import { apiMessage } from '../../services/data';
+import { IResponseMessage } from '../../services/data/Message';
 import { styles } from "./styles";
 import { useAuth } from '../../hook/auth';
 
 export function PageList() {
     const wallpapper = require('../../assets/wallpapper.png')
-    const { user, signOut } = useAuth()
+    const [message, setMessage] = useState<IResponseMessage[]>([])
+    const { setLoading } = useAuth()
+    useEffect(() => {
+        setLoading(true)
+        async function loadMessage() {
+            const response = await apiMessage.index()
+            setMessage(response.data)
+        }
+        setLoading(false)
+        loadMessage()
+    }, [])
+
+    interface itemMessage {
+        item: IResponseMessage
+    }
+    const renderItem = (({ item }: itemMessage) => {
+        return (
+            <View style={styles.item}>
+                <Text style={styles.itemText}>Título: {item.title}</Text>
+                <Text style={styles.itemText}>Mensagem: {item.message}</Text>
+            </View>
+        )
+    })
     return (
         <ImageBackground source={wallpapper} style={styleContainer.container}>
             <View style={styles.flex}>
-
+                {
+                    message.length > 0 && (
+                        <FlatList 
+                        data={message} 
+                        renderItem={renderItem} 
+                        keyExtractor={(item) => String(item.id)} />
+                    )
+                }
             </View>
         </ImageBackground>
     )
--- src/screens/PageList/styles.ts
+++ src/screens/PageList/styles.ts
@@ -6,6 +6,21 @@ export const styles = StyleSheet.create({
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center',
-        width: 350,
+        width: 350
     },
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    item: {
+        backgroundColor: colors.square,
+        width: "100%",
+        marginTop: 10,
+        borderRadius: 10
+    },
+    itemText: {
+        fontSize: 20,
+        padding: 10
+    }
 })
